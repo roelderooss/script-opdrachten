c8c5334473f0b5e920161fb73a78b689ea291ad2
roelderooss <roelderoos1234@gmail.com>
2022-10-05 12:11:03 +0200

    Heb functie toegevoegd die kan tellen hoe vaak een getal voor komt in een array.

--- yahtzee/scripts.js
+++ yahtzee/scripts.js
@@ -4,6 +4,22 @@ function Gooi() {
   for (var i = 0; i < 5; ++i) {
     dice[i] = Math.floor(Math.random() * 6) + 1;
   }
-  console.log(dice);
+  function f(array, value) {
+    var n = 0;
+    for (i = 0; i < array.length; i++) {
+      if (array[i] == value) {
+        n++;
+      }
+    }
+    return n;
+  }
+  let count = {
+    amountOfOnes: f(dice, 1),
+    amountOfTwos: f(dice, 2),
+    amountOfThrees: f(dice, 3),
+    amountOfFours: f(dice, 4),
+    amountOfFives: f(dice, 5),
+    amountOfSixs: f(dice, 6),
+  };
   return dice;
 }
