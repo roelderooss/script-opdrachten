705958673a57d6869d915492005959f8b352d01b
roelderooss <roelderoos1234@gmail.com>
2022-10-11 16:25:13 +0200

    Heb gezorgd dat nummers veranderen

--- yahtzee/scripts.js
+++ yahtzee/scripts.js
@@ -4,15 +4,7 @@ function Gooi() {
   for (var i = 0; i < 5; ++i) {
     dice[i] = Math.floor(Math.random() * 6) + 1;
   }
-  function f(array, value) {
-    var n = 0;
-    for (i = 0; i < array.length; i++) {
-      if (array[i] == value) {
-        n++;
-      }
-    }
-    return n;
-  }
+
   let count = {
     amountOfOnes: f(dice, 1),
     amountOfTwos: f(dice, 2),
@@ -21,5 +13,24 @@ function Gooi() {
     amountOfFives: f(dice, 5),
     amountOfSixs: f(dice, 6),
   };
+
+  const diceHTML = document.getElementsByClassName("dice");
+  const x = dice;
+  for (let i = 0; i < x.length; i++) {
+    for (let i = 0; i < diceHTML.length; i++) {
+      diceHTML[i].innerHTML = x[i];
+    }
+  }
+
   return dice;
+}
+
+function f(array, value) {
+  var n = 0;
+  for (i = 0; i < array.length; i++) {
+    if (array[i] == value) {
+      n++;
+    }
+  }
+  return n;
 }
